55097c1b7ec44fc0eaf94c380672ae1186aa512e
Odinaka Afocha <=>
2022-10-26 08:53:29 +0100

    first commit

---
+++ src/components/Products/ProductItem.js
@@ -0,0 +1,34 @@
+import React,{memo} from 'react';
+
+//import { ProductsContext } from '../../context/products-context';
+import Card from '../UI/Card';
+import { useStore } from '../../hook-store/store';
+import './ProductItem.css';
+
+
+const ProductItem = memo(props => {
+ //const toggleFav = useContext(ProductsContext).toggleFav;
+ const dispatch = useStore(false)[1];
+
+  const toggleFavHandler = () => {
+    //toggleFav(props.id);
+    dispatch('TOGGLE_FAV', props.id);
+  };
+
+  return (
+    <Card style={{ marginBottom: '1rem' }}>
+      <div className="product-item">
+        <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
+        <p>{props.description}</p>
+        <button
+          className={!props.isFav ? 'button-outline' : ''}
+          onClick={toggleFavHandler}
+        >
+          {props.isFav ? 'Un-Favorite' : 'Favorite'}
+        </button>
+      </div>
+    </Card>
+  );
+});
+
+export default ProductItem;
